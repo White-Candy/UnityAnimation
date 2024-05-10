359840e3dd98014a0a94086ba3c2a2784f1bc385
Sugar <287276293@qq.com>
2024-05-10 18:05:56 +0800

    feat: code push

---
+++ Assets/Scripts/Anim/AnimController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimStruct
+{
+    public string name;
+    public float f_start;
+    public float f_end;
+}
+
+public class AnimController : MonoBehaviour
+{
+    private static AnimController m_instance;
+    public static AnimController Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = new AnimController();
+            }
+            return m_instance;
+        }
+    }
+
+    public IEnumerator AnimClipPlay(GameObject obj, string stateName, AnimStruct clip, params Tuple<string, bool>[] param)
+    {
+        Animator animtor = obj?.GetComponent<Animator>(); //获得组件Animator
+
+        foreach (var p in param)
+        {
+            animtor.SetBool(p.Item1, p.Item2);  // 修改 Animtor状态机的参数
+        }
+
+        yield return new WaitForSeconds(0.01f);
+
+        if (clip != null && animtor != null)
+        {
+            float frameRate = animtor.GetCurrentAnimatorClipInfo(0)[0].clip.frameRate;
+            float start = clip.f_start * (1 / frameRate);
+            float end = clip.f_end * (1 / frameRate);
+            float animTime = (end - start); // f_start 和 f_end 两个帧时间间隔
+
+            animtor.PlayInFixedTime(stateName, 0, start); // 从 start时间开始播放动画
+            animtor.speed = 1.0f;
+
+            yield return new WaitForSeconds(animTime);
+
+            // 播放完毕关闭动画
+            animtor.speed = 0f;
+            foreach (var p in param)
+            {
+                animtor.SetBool(p.Item1, !p.Item2);
+            }
+        }
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene : MonoBehaviour
+{
+    public string animStateName;
+    public List<AnimStruct> AnimClips;
+    int clipIdx = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (clipIdx < AnimClips.Count)
+            {
+                StartCoroutine(AnimController.Instance.AnimClipPlay(this.gameObject, animStateName, AnimClips[clipIdx++],
+                                new Tuple<string, bool>("play", true)));
+            }
+        }
+    }
+}
